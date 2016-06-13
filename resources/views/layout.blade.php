471a81d9dc409d7d5c4ec464ecc470fc315d326b
Steven Medina <stevenmedina.co@gmail.com>
2016-06-13 17:00:27 -0500

    more styling

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,32 +8,56 @@
         <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        <nav class="navbar navbar-static-top navbar-dark bg-inverse">
-          <a href="/" class="navbar-brand">Rest API</a>
-          <ul class="nav navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="/">Inicio<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/user/create">Registro</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/user/">Listar Usuarios</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/documentation/">Documentación</a>
-            </li>
-          </ul>
+
+        <nav>
+            <div class="container">
+                <div class="nav-wrapper">
+                    <a href="/" class="brand-logo">Rest API</a>
+                    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="/">Inicio</a></li>
+                        <li><a href="/user/create">Registro</a></li>
+                        <li><a href="/user/">Listar Usuarios</a></li>
+                        <li><a href="/documentation/">Documentación</a></li>
+                    </ul>
+                    <ul class="side-nav" id="mobile-demo">
+                        <li><a href="/">Inicio</a></li>
+                        <li><a href="/user/create">Registro</a></li>
+                        <li><a href="/user/">Listar Usuarios</a></li>
+                        <li><a href="/documentation/">Documentación</a></li>
+                    </ul>
+                </div>
+            </div>
         </nav>
 
-        <div class="container">
-            <div class="content">
-                @yield('content')
+        <main>
+            <div class="container">
+                <div class="content">
+                    @yield('content')
+                </div>
             </div>
-        </div>
+        </main>
+
+        <footer class="page-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="white-text">Rest API</h5>
+                        <p class="grey-text text-lighten-4">Esto es una API REST Hecha en Laravel, Podras Registrarte, Iniciar sesion, Listar usuarios, y hacer peticiones para recibir Documentos JSON.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-copyright">
+                <div class="container">
+                    © 2016 Copyright Steven Medina
+                </div>
+            </div>
+        </footer>
         <div class="scripts">
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+            <script src="js/main.js"></script>
         </div>
     </body>
 </html>
