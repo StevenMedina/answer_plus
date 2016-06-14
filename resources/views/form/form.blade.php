d026624c04cd2d84c8832f758a5d49dd2cf02a3f
Steven Medina <stevenmedina.co@gmail.com>
2016-06-14 11:38:03 -0500

    Style form

--- resources/views/form/form.blade.php
+++ resources/views/form/form.blade.php
@@ -2,47 +2,51 @@
 
 @section('content')
     <div class="row">
-        <div class="jumbotron">
+        <div class="container">
             <h2>Form</h2>
+            <form class="col s12 form-register" method="post" action="{{ url('/user') }}" data-toggle="validator" role="form">
+                {{ csrf_field() }}
 
-            <form class="col s12" method="post" action="{{ url('/user') }}" data-toggle="validator" role="form">
-                <div class="row">
-                    {{ csrf_field() }}
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">label</i>
+                    <label for="name">Nombre</label>
+                    <input class="validate" type="text" id="name" name="name">
+                </div>
 
-                    <div class="input-field col s6">
-                        <i class="material-icons prefix">account_circle</i>
-                        <input class="validate" type="text" id="name" name="name" placeholder="Nombre">
-                    </div>
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">account_circle</i>
+                    <label for="user">Usuario</label>
+                    <input class="validate" type="text" id="user" name="user">
+                </div>
 
-                    <div class="input-field col s6">
-                        <i class="material-icons prefix">account_circle</i>
-                        <input class="validate" type="text" id="user" name="user" placeholder="Usuario">
-                    </div>
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">chat_bubble_outline</i>
+                    <label for="email">Correo Electronico</label>
+                    <input class="validate" type="email" id="email" name="email">
+                </div>
 
-                    <fieldset class="form-group">
-                        <label for="user">Usuario</label>
-                        <input class="form-control" type="text" id="user" name="user" placeholder="Usuario" required>
-                    </fieldset>
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">phone</i>
+                    <label for="phone">Telefono celular</label>
+                    <input class="validate" type="email" id="phone" name="phone">
+                </div>
 
-                    <fieldset class="form-group">
-                        <label for="email">Correo Electronico</label>
-                        <input class="form-control" type="email" id="email" name="email" placeholder="Correo Electronico" required>
-                    </fieldset>
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">lock_outline</i>
+                    <label for="password">Contraseña</label>
+                    <input class="validate" type="password" id="password" name="password">
+                </div>
 
-                    <fieldset class="form-group">
-                        <fieldset class="form-group">
-                            <label for="password">Contraseña</label>
-                            <input class="form-control" type="password" id="inputPassword" data-minlength="6" name="password" placeholder="Contraseña" required>
-                            <div class="help-block">Minimo 6 caracteres</div>
-                        </fieldset>
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">lock_outline</i>
+                    <label for="r_password">Repetir contraseña</label>
+                    <input class="validate" type="password" id="r_password">
+                </div>
 
-                        <fieldset class="form-group">
-                            <label for="r_password">Repetir Contraseña</label>
-                            <input class="form-control" type="password" id="r_password" placeholder="Repetir Contraseña" data-match="#inputPassword" data-match-error="Whoops, these don't match" required>
-                        </fieldset>
-                    </fieldset>
-
-                    <button type="submit" class="btn btn-success" type="button">Enviar</button>
+                <div class="input-field">
+                    <button class="btn waves-effect waves-light" type="submit" name="action">Enviar
+                        <i class="material-icons right">send</i>
+                    </button>
                 </div>
             </form>
         </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -58,7 +58,8 @@
         <div class="scripts">
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-            <script src="js/main.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+            <script src="/js/main.js"></script>
         </div>
     </body>
 </html>
